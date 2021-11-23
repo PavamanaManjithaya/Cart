fa59fdcc8080ad30fef23757d013aab2563723a7
pavamana manjithaya <18pavamanamanjithaya@gmail.com>
2021-11-23 17:01:16 +0530

    deploy commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\food;
+use App\Models\Food;
 class Category extends Model
 {
     use HasFactory;
     protected $fillable = ['name'];
     public function food(){
-        return $this->hasOne(food::class,'category_id','id');
+        return $this->hasOne(Food::class,'category_id','id');
     }
 }
